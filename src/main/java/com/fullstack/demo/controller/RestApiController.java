8d94f92e34b299be958d7fdaeb1de425186c7f77
Kala <jkala@ms.ds.uhc.com>
2019-01-04 17:37:27 +0530

    First commit

---
+++ src/main/java/com/fullstack/demo/controller/RestApiController.java
@@ -0,0 +1,14 @@
+package com.fullstack.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApiController {
+	
+	@RequestMapping("/api")
+	public String hi(){
+		return "Hello world! >>> Message from <a href='https://grokonez.com' target='_blank'>grokonez.com</a>";
+	}
+
+}
